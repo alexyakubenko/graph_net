66f4b90ad4c423fd51fb168c3ba4923a03f9f2c7
Alexandr Yakubenko <alexandr.yakubenko@gmail.com>
2014-11-30 16:06:21 +0300

    added ability to show profiles of user users

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,4 +1,5 @@
 class RecommendationsController < ApplicationController
   def index
+    @recommendations = Neo4j::Session.query("MATCH (n) WHERE n._classname <> 'Attribute' AND ID(n) <> {my_id} RETURN n;", my_id: current_user.id)
   end
 end
