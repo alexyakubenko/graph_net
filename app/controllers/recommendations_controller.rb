ec170463dd9c949c3b78d2c2f7dd33e3cda04fc4
Alexandr Yakubenko <alexandr.yakubenko@gmail.com>
2014-11-30 21:00:45 +0300

    do not recommend myself

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,5 +1,5 @@
 class RecommendationsController < ApplicationController
   def index
-    @recommendations = Neo4j::Session.query("MATCH (n) WHERE n._classname <> 'Attribute' AND ID(n) <> {my_id} RETURN n;", my_id: current_user.id)
+    @recommendations = Neo4j::Session.query("MATCH (n) WHERE n._classname <> 'Attribute' AND n.uuid <> {my_id} RETURN n;", my_id: current_user.id)
   end
 end
